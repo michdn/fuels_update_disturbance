f3b4f4dd51bcb3901787e174e7a13800ec9ae261
Dawn Nekorchuk <28901045+michdn@users.noreply.github.com>
2026-03-03 08:09:38 -0800

    Finished LF disturbance to DIST code script

--- scripts/disturbance/1_LF_annual_dist_to_code.R
+++ scripts/disturbance/1_LF_annual_dist_to_code.R
@@ -35,12 +35,12 @@ source(file.path("scripts", "common_vars_functions.R"))
 source(file.path("scripts", "0_parameters", "2026_WRME_LF2024_updt2025.R"))
 
 ## LANDFIRE disturbance
-# version_dist is the LF year
-version_dist <- "2025"
+# dist_year is the LF year
+dist_year <- "2025"
 # dist_vers is the version of disturbance data c("LDist", "PDist", "Dist")
 #  "Limited": "LDist", 
 #  "Preliminary": "PDist", 
-#   "Annual": "Dist"
+#  "Annual": "Dist"
 dist_vers <- "LDist"
 
 ### D_TYPE & D_SEVERITY xwalks -------------------------
@@ -49,7 +49,7 @@ dist_vers <- "LDist"
 # 1: Fire, 2: Mechanical Add, 3: Mechanical Remove, 4: Windthrow, 
 # 5: Insects-Disease, 6: Mechanical Unknown, 7: Mastication
 dtype_xwalk <- tribble(
-  ~type_dist, ~type_code,
+  ~DIST_TYPE, ~D_TYPE,
   "Fire", 1, 
   "Wildfire", 1, 
   "Wildland Fire Use", 1, 
@@ -75,7 +75,7 @@ dtype_xwalk <- tribble(
 #D_SEVERITY, 2nd digit
 # 1: Low, 2: Moderate, 3: High
 dsev_xwalk <- tribble(
-  ~level_sev, ~sev_code, 
+  ~SEVERITY, ~D_SEVERITY, 
   #"Unburned/Low", "?", #dropping 
   "Low", 1, 
   "Moderate", 2, 
@@ -84,52 +84,123 @@ dsev_xwalk <- tribble(
 #dropping "No Severity", "Increased Green"
 
 #D_TIME
-# Is most likely always 1 if doing recent updates
+# 1 if doing recent updates. Not fully built to handle multiple years.
 dtime_xwalk <- tibble(
-  years_time = 1:10, 
-  time_code = c(1, rep(2, 4), rep(3, 5)))
-
+  diff_years = 1:10, 
+  D_TIME = c(1, rep(2, 4), rep(3, 5)))
 
 ### Data in ------------------------------------------
 
 #folder and file name depends on the LF year and dist version
+# eg limited like LF2025_LDist25_CONUS.tif
+# prelim like LF2024_PDist24_CONUS.tif
+# final annual like LF2024_Dist24_CONUS.tif
+# HOWEVER, if downloading from compilations, the file names are different!
+#  this script does not handle these (eg LC24_Dist_250.tif), ONLY yearly ones. 
 
+# Find targets based on dist_year and dist_vers
+fname_base <- paste0("LF", dist_year,  
+                     "_", dist_vers, 
+                     #last two digits of year
+                     str_sub(dist_year, start=-2, end=-1))
 
-#csv crosswalk
-
-
-
-# Get NoData (-9999), NA (0), and Water mask value (sometimes 2001)
-
-
-# Find target based on version_target
-
+#csv data from landfire
 file_dist_csv <- list.files(
-  path = folder_landfire,
-  pattern = paste0("^LF", version_target, 
-                   #last two digits of year
-                   "_Dist", str_sub(version_target, start=-2, end=-1),
-                   "_CONUS\\.tif$"),
-  full.names = TRUE)
+  path = folder_lfdata_base,
+  pattern = paste0(fname_base, "\\.csv$"), 
+  full.names = TRUE,
+  recursive = TRUE)
+dist_csv <- read_csv(file_dist_csv)
 
-bps <- rast(file_bps)
-
-
+#disturbance raster
+file_dist_rast <- list.files(
+  path = folder_lfdata_base,
+  pattern = paste0(fname_base, "_CONUS\\.tif$"), 
+  full.names = TRUE,
+  recursive = TRUE)
+dist_r <- rast(file_dist_rast)
 
 
 ### Dynamic crosswalk to DIST codes ------------------
 
-# Descriptions and other column values haven't changed. 
-#  If they do, these mini xwalks will need to be updated/dynamic.
+#time component (single year only)
+yr_diff <- as.numeric(dist_year) - as.numeric(version_target)
 
+# disturbance type and severity
+# Descriptions and other column values haven't changed from LF dist versions. 
+#  If they do, the mini xwalks above will need to be updated/dynamic.
+dist_dist <- dist_csv %>% 
+  dplyr::select(VALUE, DIST_TYPE, SEVERITY) %>% 
+  #add year difference from LF version and disturbance version, calc above
+  mutate(diff_years = .env$yr_diff) %>% 
+  #join all xwalks, dropping those unmatched (don't want NAs)
+  inner_join(dtype_xwalk, by = join_by(DIST_TYPE)) %>% 
+  inner_join(dsev_xwalk, by = join_by(SEVERITY)) %>% 
+  inner_join(dtime_xwalk, by = join_by(diff_years)) %>% 
+  # make DIST code
+  mutate(DIST = paste0(D_TYPE, D_SEVERITY, D_TIME) %>% as.numeric())
 
+# Get NoData (-9999), NA (0), and Water mask value (sometimes 2001)
+dist_spec <- dist_csv %>% 
+  filter(DIST_TYPE %in% c("Fill-NoData")) %>% 
+  mutate(DIST = NA_real_) %>% 
+  rows_append(dist_csv %>% 
+                filter(DIST_TYPE %in% c(NA, "Water")) %>% 
+                mutate(DIST = 0)) %>% 
+  dplyr::select(VALUE, DIST_TYPE, SEVERITY, DIST)
+
+# Full xwalk for reclassifying (and checking)
+dist_codes <- bind_rows(dist_spec, dist_dist)
+
+# TODO
+# NOTE: This will silently drop things that aren't in mini crosswalks above
+#  Probably want to build out something more robust or write out unmatched to 
+#   review. Some are intentionally dropped (low/unburned, etc.)
 
 ### Reclassify LF disturbance to DIST codes ----------
 
+dist_rcl <- dist_codes %>% 
+  dplyr::select(VALUE, DIST) %>% 
+  as.matrix()
+
+activeCat(dist_r) <- 0
+dist_r
+dist_r_coded <- classify(dist_r, rcl = dist_rcl, 
+                         #NA dropping everything not in our xwalk! Intentional.
+                         others = NA)
+names(dist_r_coded) <- "DIST"
+varnames(dist_r_coded) <- "DIST"
+dist_r_coded
+
+# #testing with classify others = -1
+# dist_r_coded <- classify(dist_r, rcl = dist_rcl, others = -1)
+# fail_r <- mask(dist_r, dist_r_coded, maskvalue = -1, inverse = TRUE)
+# fail_r
+# fail_freq <- freq(fail_r)
+# fail_freq %>% as_tibble() %>% arrange(desc(count))
+# # all unburned/low or increased green, all good. 
+# writeRaster(
+#   fail_r, 
+#   file.path("data", "test_data", "lf_dist_qa", "fail_r.tif"),
+#   gdal=c("COMPRESS=DEFLATE"),
+#   overwrite = TRUE)
+# writeRaster(
+#   dist_r_coded, 
+#   file.path("data", "test_data", "lf_dist_qa", "dist_r_coded.tif"),
+#   gdal=c("COMPRESS=DEFLATE"),
+#   overwrite = TRUE)
 
 
 # Save out
 
+folder_out <- file.path(folder_out_base, version_proj, "disturbance")
+dir.create(folder_out, showWarnings = FALSE, recursive=TRUE)
+
+writeRaster(
+  dist_r_coded,
+  file.path(folder_out, 
+            paste0(dist_vers, "_", dist_year, "_dist_coded.tif")),
+  gdal=c("COMPRESS=DEFLATE"))
 
 
 
