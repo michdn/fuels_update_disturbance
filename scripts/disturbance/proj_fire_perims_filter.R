92823fbdb021db0ca06233dbb481d91a9bc1d8d3
Dawn Nekorchuk <28901045+michdn@users.noreply.github.com>
2026-03-18 10:46:21 -0700

    Project-specific script for filtering which fire perimeters to use for estimating burn severity (outside analysis)

---
+++ scripts/disturbance/proj_fire_perims_filter.R
@@ -0,0 +1,48 @@
+# Filtering fire perimeters
+#  (from burn-severity-gee pull nifc fires)
+
+# From yearly 2025 to only 2025 post end of fiscal year
+#   (LF products run to fiscal year)
+# So Oct-December fires
+
+# Will upload these Oct-Dec perimeters to GEE
+#  to use burn-severity-gee to calculate
+#  estimated burn severity
+
+# Then we will use another script(s) here
+#  to create DIST codes and combine with LF disturbances.
+
+### Packages & Function -------------------------------
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+  tidyverse,
+  sf)
+
+source(file.path("scripts", "common_vars_functions.R"))
+
+### User settings -------------------------------------
+
+source(file.path("scripts", "0_parameters", "2026_WRME_LF2024_updt2025.R"))
+
+### Data in -------------------------------------------
+
+perims <- read_sf(file.path(
+  folder_out_base, version_proj, "disturbance", "fire_perims",
+  "nifc_fires_2025_gte100ac_20260313.shp"))
+
+### Filter and save -----------------------------------
+
+#post fiscal year
+# MAKE SURE to keep Discovery field as character! 
+pfy25 <- perims %>% 
+  mutate(Disc_date = as.Date(Discovery)) %>% 
+  filter(Disc_date >= as.Date("2025-10-01")) %>% 
+  arrange(Disc_date) %>% 
+  dplyr::select(-Disc_date)
+
+write_sf(pfy25,
+         file.path(
+           folder_out_base, version_proj, 
+           "disturbance", "fire_perims",
+           "nifc_fires_2025postFY_gte100ac_20250313.shp"))
